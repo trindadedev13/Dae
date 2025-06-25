09d1d3cf84dbe619ec4810ab6390f9512e56d4ea
trindadedev13 <trindadedev13@gmail.com>
2025-06-25 16:56:15 -0300

    feat(build): Improve Build, Add Option to Build Some ABIS

--- Make.sh
+++ Make.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-INSTALL_P=false
-RUN_P=false
+INSTALL_P=OFF
+RUN_P=OFF
 
-TERMUX_P=false
-ASAN_P=false
-CLEAN_P=true
+TERMUX_P=OFF
+ASAN_P=OFF
+CLEAN_P=OFF
+LIB_SO_P=OFF
 
 YELLOW="\e[33m"
 LGREEN="\e[92m"
@@ -20,12 +21,13 @@ print_help_and_close() {
   echo -e "$RESET"
   echo "Usage Make.sh <option>"
   echo "Options:"
-  echo -e "$LGREEN-i  or --install | $LMAGENTA Compile and install."
-  echo -e "$LGREEN-r  or --run     | $LMAGENTA Compile and run."
-  echo -e "$LGREEN-t  or --termux  | $LMAGENTA [USE WITH -r] Compile and run it fixing termux restrictions."
-  echo -e "$LGREEN-h  or --help    | $LMAGENTA Prints help."
-  echo -e "$LGREEN-as or --asan    | $LMAGENTA Enables Address Sanitizer."
-  echo -e "$LGREEN-c  or --clean   | $LMAGENTA Cleanup build before build again."
+  echo -e "$LGREEN-i    or  --install | $LMAGENTA Compile and install."
+  echo -e "$LGREEN-r    or  --run     | $LMAGENTA Compile and run."
+  echo -e "$LGREEN-t    or  --termux  | $LMAGENTA [USE WITH -r] Compile and run it fixing termux restrictions."
+  echo -e "$LGREEN-h    or  --help    | $LMAGENTA Prints help."
+  echo -e "$LGREEN-as   or  --asan    | $LMAGENTA Enables Address Sanitizer."
+  echo -e "$LGREEN-c    or  --clean   | $LMAGENTA Cleanup build before build again."
+  echo -e "$LGREEN-lso  or  --libso   | $LMAGENTA Cleanup build before build again."
   echo ""
   echo -e "$YELLOW"
   echo "WARNING"
@@ -37,34 +39,40 @@ print_help_and_close() {
 for arg in "$@"; do
   case "$arg" in
     -i)
-      INSTALL_P=true
+      INSTALL_P=ON
       ;;
     --install)
-      INSTALL_P=true
+      INSTALL_P=ON
       ;;
     -r)
-      RUN_P=true
+      RUN_P=ON
       ;;
     --run)
-      RUN_P=true
+      RUN_P=ON
       ;;
     -t)
-      TERMUX_P=true
+      TERMUX_P=ON
       ;;
     --termux)
-      TERMUX_P=true
+      TERMUX_P=ON
       ;;
     -as)
-      ASAN_P=true
+      ASAN_P=ON
       ;;
     --asan)
-      ASAN_P=true
+      ASAN_P=ON
       ;;
     -c)
-      CLEAN_P=true
+      CLEAN_P=ON
       ;;
     --clean)
-      CLEAN_P=true
+      CLEAN_P=ON
+      ;;
+    -lso)
+      LIB_SO_P=ON
+      ;;
+    --libso)
+      LIB_SO_P=ON
       ;;
     -h)
       print_help_and_close
@@ -78,44 +86,57 @@ for arg in "$@"; do
   esac
 done
 
-if [ "$ASAN_P" = true ]; then
-  export ASAN=true
+if [ "$ASAN_P" = ON ]; then
+  export ASAN=ON
 fi
 
-if [ "$CLEAN_P" = true ]; then
+if [ "$CLEAN_P" = ON ]; then
   rm -rf build
 fi
 
-# Creates build dir (if needed) and enter in it
 mkdir -p build
-cd build
 
 # Create CMake Build files based on INSTALL option
-if [ "$INSTALL_P" = true ]; then
-  export INSTALL=$INSTALL_P
-  cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/../usr
+if [ "$LIB_SO_P" = ON ]; then
+  ABIS=("armeabi-v7a" "arm64-v8a" "x86" "x86_64")
+  for ABI in "${ABIS[@]}"; do
+    BUILD_DIR=build/$ABI
+
+    cmake -B $BUILD_DIR \
+      -DASAN=$ASAN_P \
+      -DINSTALL=$INSTALL_P \
+      -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+      -DANDROID_ABI=$ABI \
+      -DANDROID_PLATFORM=android-26 \
+      -DCMAKE_BUILD_TYPE=Release
+
+    cmake --build $BUILD_DIR
+  done
 else
-  cmake ..
+  cmake -B build \
+    -DASAN=$ASAN_P \
+    -DINSTALL=$INSTALL_P \
+    -DCMAKE_INSTALL_PREFIX=$HOME/../usr
 fi
 
 # Finally build
-make
+cmake --build build
 
-# Install program if install is true
-if [ "$INSTALL_P" = true ]; then
-  cmake --install .
+# Install program if install is ON
+if [ "$INSTALL_P" = ON ]; then
+  cmake --install build
 fi
 
-# Runs program if install option is false
-if [ "$INSTALL_P" = false ]; then
-  if [ "$RUN_P" = true ]; then
-    if [ "$TERMUX_P" = true ]; then
+# Runs program if install option is OFF
+if [ "$INSTALL_P" = OFF ]; then
+  if [ "$RUN_P" = ON ]; then
+    if [ "$TERMUX_P" = ON ]; then
       cp kilate $HOME
       cd $HOME
       chmod +x kilate
     else
       chmod +x kilate
     fi
-    ./kilate run main.kilate
+    ./kilate run main.klt
   fi
 fi
